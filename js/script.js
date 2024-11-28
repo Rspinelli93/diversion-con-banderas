de5437363b8ae57e81715e68c97261bbc87d66de
RiccardoS <rspinellir13@gmail.com>
2024-11-28 17:59:53 +0100

    Ejercicio Terminado

--- js/script.js
+++ js/script.js
@@ -1,94 +1,99 @@
-
-const listaPaises = document.getElementById('countries-list')
+const listaPaises = document.getElementById('countries-list');
 
 // --- FETCH ---//
 
 const paises = async () => {
-   try {
-    const response = await fetch ('https://restcountries.com/v3/all')
+  try {
+    const response = await fetch('https://restcountries.com/v3/all');
     if (!response.ok) {
-        throw new Error('No se ah podido cargar la api')
-       }
-    const data = await response.json()
-    return data
-   } 
-   catch(error) {
-    console.error(error)
-   }
-}
+      throw new Error('No se ha podido cargar la API');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-// --- FUNCION NOMBRE + BANDERA --- //
+// --- VENTANA --- //
+
+const ventanaInfo = (data) => {
+  const ventana = document.querySelector('.ventana');
+  
+  ventana.innerHTML = '';
+
+  const boton = document.createElement('button');
+  boton.className = 'botonEliminar';
+  boton.innerText = 'Cerrar';
+
+  const ulInfo = document.createElement('ul');
+  ulInfo.className = 'listaInfo';
+  ulInfo.innerHTML = `
+    <li id='paisVentana'>${data.name.common}</li>
+    <li>Capital: ${data.capital?.[0] || 'N/A'}</li>
+    <li>Population: ${data.population.toLocaleString()} hab.</li>
+    <li>Driving Side: ${data.car.side}</li>
+  `;
+
+  const bandera = document.createElement('img');
+  bandera.src = data.flags[0];
+  bandera.className = 'bandera-ventana';
+  bandera.alt = `${data.name.common} Flag`;
+
+  const divCansado = document.createElement('div')
+  divCansado.className = 'div-cansado'
+
+  ventana.appendChild(divCansado);
+  ventana.appendChild(boton);
+  
+  divCansado.appendChild(bandera);
+  divCansado.appendChild(ulInfo);
+ 
+  ventana.style.display = 'flex';
+
+  // --- BOTON CERRAR --- //
+  boton.addEventListener('click', () => {
+    ventana.style.display = 'none';
+  });
+};
+
+// --- FUNCION PAISES DOM --- //
 
 const paisesBox = (data) => {
-
   data.sort((a, b) => a.name.common.localeCompare(b.name.common));
-   
-  data.forEach(element => {
-    
+
+  data.forEach((element) => {
     const liPaises = document.createElement('li');
-     
+
     const bandera = document.createElement('img');
     bandera.src = element.flags[0];
     bandera.className = 'bandera';
     bandera.alt = `${element.name.common} Flag`;
-  
 
     const divPais = document.createElement('div');
     divPais.className = 'pais';
-    divPais.textContent = element.name.common; 
+    divPais.textContent = element.name.common;
 
-    
     liPaises.appendChild(bandera);
     liPaises.appendChild(divPais);
-  
-    listaPaises.appendChild(liPaises);
 
-    // -- Evento Agrandar cuadro -- //
+    // --- EVENTO EN BANDERA --- //
 
     bandera.addEventListener('click', () => {
-    
-      liPaises.classList.add('animacion');
-      divPais.remove()
-      
-      const boton = document.createElement('button')
-      boton.className = 'botonEliminar'
-      boton.innerText = 'Cerrar'
-
-      const ulInfo = document.createElement('ul')
-      ulInfo.className = 'listaInfo';
-
-      ulInfo.innerHTML = `
-        <li>Country: ${element.name.common}</li>
-        <li>Capital: ${element.capital[0]}</li>
-        <li>Population: ${element.population} hab.</li>
-        <li>Driving Side: ${element.car.side}</li>
-      `
-      liPaises.appendChild(ulInfo)
-      liPaises.appendChild(boton)
-
-      // -- Evento Achicar cuadro -- //
-      
-      boton.addEventListener('click', () => {
-        liPaises.appendChild(divPais);
-
-        liPaises.classList.remove('animacion');
-      
-        ulInfo.remove();
-        boton.remove();
-      })
+      ventanaInfo(element);
     });
 
-    
-  }) 
-}
+    listaPaises.appendChild(liPaises);
+
+  });
+};
 
 // --- FUNCION PRINCIPAL --- //
-  
-paises()
-  .then(data => {
-    paisesBox(data);
-    console.log(data)
-    return data
-})
 
-// -------------------------------------------- //
+paises()
+  .then((data) => {
+    paisesBox(data);
+  })
+  .catch((error) => console.error(error));
+
+  // -------------------------------------------- //
